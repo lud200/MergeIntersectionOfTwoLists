c9572816e24a235f75e7847bd6eff75b32a2ac2e
dludaya <dludaya.cs@gmail.com>
2014-03-08 15:20:31 -0500

    Merge_update

--- MergeIntersection/src/Merge/merge.java
+++ MergeIntersection/src/Merge/merge.java
@@ -9,15 +9,18 @@ public class merge {
 		l1.add(2);
 		l1.add(4);
 		l1.add(5);
-		l2.add(2);
+		l2.add(1);
 		l2.add(4);
 		l2.add(3);
+		l2.add(9);
 		l2.add(2);
 		HashMap h=new HashMap();
 		HashMap h1=new HashMap();
-		//int i=l1.size()>l2.size()?l1.size():l2.size();
-		for(int i=0; i<l1.size(); i++){
+		int n=l1.size()>l2.size()?l1.size():l2.size();
+		for(int i=0;i<l1.size(); i++){
 			h.put(l1.get(i), true);
+		}
+		for(int i=0; i<n; i++){
 			if(h.containsKey(l2.get(i))){
 				h1.put(l2.get(i), true);
 			}
